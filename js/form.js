f8da2cdeafb6a3d5f1bcc98c161883e4f72b30f6
samuelcodingg <samuelroman39@gmail.com>
2021-07-22 14:30:05 -0500

    se hizo validaciones al click del boton, falta solo enviar

--- js/form.js
+++ js/form.js
@@ -5,10 +5,10 @@ const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
 const enviar = document.querySelector('#enviar');
 
-
 //cuando se cargue el DOM
 
 document.addEventListener('DOMContentLoaded', function(){
+
     //Validar nombre
     nombre.addEventListener('blur', validarNombre);
 
@@ -22,7 +22,7 @@ document.addEventListener('DOMContentLoaded', function(){
     mensaje.addEventListener('blur',validarMensaje);
 
     //Validar enviar
-    enviar.addEventListener('click',validarEnviar);
+    enviar.addEventListener('click', validarEnviar);
 });
 
 function validarNombre(e){   
@@ -30,15 +30,15 @@ function validarNombre(e){
     // si está vacío para
     if(!cumpleLongitud(e.target.value)){
 
-        if(e.target.nextSibling.tagName != 'P'){ 
+        if(e.target.nextElementSibling.tagName != 'P'){ 
             mensajeError(e.target, 'Por favor, ingresa tu nombre');
         }
 
         return false;
     }
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     return true;
@@ -47,15 +47,15 @@ function validarNombre(e){
 function validarAsunto(e){
     if(!cumpleLongitud(e.target.value)){
 
-        if(e.target.nextSibling.tagName != 'P'){ 
+        if(e.target.nextElementSibling.tagName  != 'P'){ 
             mensajeError(e.target, 'Por favor, ingresa el asunto');
         }
 
         return false;
     }
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName  == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     return true;
@@ -64,15 +64,15 @@ function validarAsunto(e){
 function validarMensaje(e){
     if(!cumpleLongitud(e.target.value)){
 
-        if(e.target.nextSibling.tagName != 'P'){ 
+        if(e.target.nextElementSibling.tagName  != 'P'){ 
             mensajeError(e.target, 'Por favor, ingresa el mensaje');
         }
 
         return false;
     }
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName  == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     return true;
@@ -80,43 +80,96 @@ function validarMensaje(e){
 
 function validarEmail(e){
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName  == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     if(!cumpleLongitud(e.target.value)){
 
-        if(e.target.nextSibling.tagName != 'P'){ 
+        if(e.target.nextElementSibling.tagName  != 'P'){ 
             mensajeError(e.target, 'Por favor, ingresa el email');
         }
 
         return false;
     }
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName  == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(e.target.value)){
-        if(e.target.nextSibling.tagName != 'P'){ 
+        if(e.target.nextElementSibling.tagName  != 'P'){ 
             mensajeError(e.target, 'Por favor, ingresa un email válido');
         }
         return false;
     }
 
-    if(e.target.nextSibling.tagName == 'P'){
-        e.target.nextSibling.remove();
+    if(e.target.nextElementSibling.tagName  == 'P'){
+        e.target.nextElementSibling.remove();
     }
 
     return true;
 }
 
-function validarEnviar(e) {
+function validarEnviar(e){
     e.preventDefault();
 
+    //nombre
+    if(!cumpleLongitud(nombre.value)){
+        if(nombre.nextElementSibling.tagName != 'P'){ 
+            mensajeError(nombre, 'Por favor, ingresa tu nombre');
+        }
+        return;
+    }
+    
+    //email
+    if(email.nextElementSibling.tagName  == 'P'){
+        email.nextElementSibling.remove();
+    }
 
+    if(!cumpleLongitud(email.value)){
 
+        if(email.nextElementSibling.tagName  != 'P'){ 
+            mensajeError(email, 'Por favor, ingresa el email');
+        }
 
+        return false;
+    }
+
+    if(email.nextElementSibling.tagName  == 'P'){
+        email.nextElementSibling.remove();
+    }
+
+    if (!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email.value)){
+        if(email.nextElementSibling.tagName  != 'P'){ 
+            mensajeError(email, 'Por favor, ingresa un email válido');
+        }
+        return false;
+    }
+
+    if(email.nextElementSibling.tagName  == 'P'){
+        email.nextElementSibling.remove();
+    }
+
+    if(!cumpleLongitud(asunto.value)){
+
+        if(asunto.nextElementSibling.tagName  != 'P'){ 
+            mensajeError(asunto, 'Por favor, ingresa el asunto');
+        }
+
+        return false;
+    }
+
+    if(!cumpleLongitud(mensaje.value)){
+
+        if(mensaje.nextElementSibling.tagName  != 'P'){ 
+            mensajeError(mensaje, 'Por favor, ingresa el mensaje');
+        }
+
+        return false;
+    }
+
+    
 }
 
 function cumpleLongitud(palabra){
